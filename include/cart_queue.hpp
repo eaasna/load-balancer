4ccb28926faeaa442a7295a23f3e5a50f3292d3b
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-11-28 14:11:01 +0100

    fix: add locks and a dequeue function

--- include/cart_queue.hpp
+++ include/cart_queue.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <string>
@@ -16,6 +17,11 @@ struct cart_queue {
     std::vector<secured_cart> carts_being_filled;
     size_t cart_max_capacity;
 
+    std::mutex filled_carts_mutex;
+    std::condition_variable filled_carts_cv;
+    std::vector<std::tuple<size_t, std::vector<std::string>>> filled_carts;
+    size_t max_queued_carts;
+
     cart_queue(size_t number_of_bins, size_t cart_max_capacity)
         : carts_being_filled(number_of_bins)
         , cart_max_capacity{cart_max_capacity}
@@ -30,12 +36,26 @@ struct cart_queue {
         assert(carts_being_filled.size() < bin_id && "bin_id has to be between 0 and number_of_bins");
 
         auto& cart = carts_being_filled.at(bin_id);
+        auto _ = std::lock_guard{cart.mutex};
 
         assert(cart.queries.size() < cart_max_capacity && "carts should always have at least one empty value");
         cart.queries.emplace_back(std::move(query));
         if (cart.queries.size() == cart_max_capacity) {
-            //!TODO do something more useful
+            auto _ = std::unique_lock{filled_carts_mutex};
+            filled_carts.emplace_back(bin_id, std::move(cart.queries));
+            cart.queries.reserve(cart_max_capacity);
+            filled_carts_cv.notify_one();
         }
+    }
+
+    auto dequeue() -> std::tuple<int ,std::vector<std::string>> {
+        auto g = std::unique_lock{filled_carts_mutex};
+        if (filled_carts.empty()) {
+            filled_carts_cv.wait(g);
+        }
+        auto v = std::move(filled_carts.back());
+        filled_carts.pop_back();
+        return v;
     }
 };
 
